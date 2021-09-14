5ddd693972b7c6884af8c565ca13ec8a6f32c579
Patrick <patrickrafael05@gmail.com>
2021-09-13 21:26:20 -0300

    Puxando a lista do Firebase

--- app/src/main/java/com/example/phisiotips/activity/activity/activitys/MainActivity.java
+++ app/src/main/java/com/example/phisiotips/activity/activity/activitys/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.phisiotips.activity.activity.activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,8 +18,11 @@ import com.example.phisiotips.activity.activity.config.ConfiguracaoFireBase;
 import com.example.phisiotips.activity.activity.model.MainEnqutes;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,8 +30,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button botaoSair;
-    private FirebaseAuth autenticacao;
+    //private Button botaoSair;
+    //private FirebaseAuth autenticacao;
     private RecyclerView recyclerViewMain;
     private DatabaseReference database = FirebaseDatabase.getInstance().getReference();
     private List<MainEnqutes> listaEnquete = new ArrayList<>();
@@ -38,19 +42,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        autenticacao = ConfiguracaoFireBase.getFireBaseAutenticacao();
-        botaoSair = findViewById(R.id.buttonCadastro);
+       // autenticacao = ConfiguracaoFireBase.getFireBaseAutenticacao();
+        //botaoSair = findViewById(R.id.buttonCadastro);
         recyclerViewMain = findViewById(R.id.recyclerViewMain);
 
-       // database = FirebaseDatabase.getInstance().getReference("Enquetes");
 
+        //autenticacao.signOut();
 
         //Criar filmes
-        this.criarEnquetes();
+        //this.criarEnquetes();
 
 
         //Configurar Adater
-        Adapter adapter = new Adapter( listaEnquete );
+        Adapter adapter = new Adapter(listaEnquete);
 
 
         //Configurar RecyclerView
@@ -59,6 +63,31 @@ public class MainActivity extends AppCompatActivity {
         recyclerViewMain.setHasFixedSize(true);
         recyclerViewMain.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayout.VERTICAL));
         recyclerViewMain.setAdapter(adapter);
+
+
+        //Pegando do banco de dados
+        database = FirebaseDatabase.getInstance().getReference().child("Enquetes");
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                    MainEnqutes enquetes = dataSnapshot.getValue(MainEnqutes.class);
+
+                    listaEnquete.add(enquetes);
+
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 // Botão Sair
@@ -76,13 +105,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     //Criar Lista de Enquetes
-    public void criarEnquetes(){
+   /* public void criarEnquetes() {
 
-        MainEnqutes enquete = new MainEnqutes("#LowCarb","Pouco carbohidrato pra ficar giga ");
-        this.listaEnquete.add( enquete );
-
-        enquete = new MainEnqutes("#BotaOShape", "Ficar giga usando veneno e tudo mais");
+        MainEnqutes enquete = new MainEnqutes("#LowCarb", "Pouco carbohidrato pra ficar giga ");
         this.listaEnquete.add(enquete);
 
         enquete = new MainEnqutes("#BotaOShape", "Ficar giga usando veneno e tudo mais");
@@ -97,7 +124,10 @@ public class MainActivity extends AppCompatActivity {
         enquete = new MainEnqutes("#BotaOShape", "Ficar giga usando veneno e tudo mais");
         this.listaEnquete.add(enquete);
 
+        enquete = new MainEnqutes("#BotaOShape", "Ficar giga usando veneno e tudo mais");
+        this.listaEnquete.add(enquete);
 
-    }
+
+    }*/
 
 }
