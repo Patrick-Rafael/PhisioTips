9625929879ac70b354848dd97c73494b5d2c819b
Patrick <patrickrafael05@gmail.com>
2021-10-04 20:30:44 -0300

    Mudando a adição de comentários

--- app/src/main/java/com/example/phisiotips/activity/activity/activitys/ComentariosActivity.java
+++ app/src/main/java/com/example/phisiotips/activity/activity/activitys/ComentariosActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +21,7 @@ import com.example.phisiotips.activity.activity.adpter.AdapterComentarios;
 import com.example.phisiotips.activity.activity.model.MainEnqutes;
 import com.example.phisiotips.activity.activity.model.Respostas;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,10 +34,13 @@ import java.util.List;
 public class ComentariosActivity extends AppCompatActivity {
 
     private RecyclerView recyclerComentarios;
-    private FloatingActionButton buttonComentarios;
+    //private FloatingActionButton buttonComentarios;
     private List<Respostas> listaRespostas = new ArrayList<>();
     private DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference comentarioReferenica;
     private TextView titulo;
+    private ImageButton buttonEnviar;
+    private TextInputEditText textComentario;
 
 
     @Override
@@ -50,20 +55,36 @@ public class ComentariosActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         String chave = bundle.getString("Chave");
         String textTitulo = bundle.getString("Titulo");
-        Log.i("Teste", "Chave: " + chave);
+
+        Log.i("texte", "chave: " + chave);
 
 
+        //Encaminhar para o nó do FireBase
+        comentarioReferenica = FirebaseDatabase.getInstance().getReference().child("Enquetes").child(chave).child("Resposta");
+
+        //Pegando Titulo da postagem
         titulo.setText(textTitulo);
 
 
 
         //Enviar para activity de adicionar comentarios
-        buttonComentarios.setOnClickListener(v -> {
+        /*buttonComentarios.setOnClickListener(v -> {
             Intent intent = new Intent(ComentariosActivity.this, AdicionarComentariosActivity.class);
             intent.putExtra("chave", chave);
             startActivity(intent);
 
+        });*/
+
+        //Envia o comentario
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarComentario();
+                textComentario.setText("");
+                listaRespostas.clear();
+            }
         });
+
 
 
         recyclerComentarios = findViewById(R.id.recyclerComentarios);
@@ -84,6 +105,7 @@ public class ComentariosActivity extends AppCompatActivity {
         //Puxando a lista do Firebase e exibindo no RecyclerView
 
         database = FirebaseDatabase.getInstance().getReference().child("Enquetes").child(chave).child("Resposta");
+
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -93,6 +115,7 @@ public class ComentariosActivity extends AppCompatActivity {
                     Respostas respostas = dataSnapshot.getValue(Respostas.class);
 
                     listaRespostas.add(respostas);
+
 
                 }
 
@@ -109,9 +132,33 @@ public class ComentariosActivity extends AppCompatActivity {
 
     }
 
+
+    //Envia a resposta para o nó do FireBase
+    public void enviarComentario() {
+
+        String resposta = textComentario.getText().toString();
+
+        if (!resposta.isEmpty()) {
+
+            Respostas respostas = new Respostas(resposta);
+
+            comentarioReferenica.push().setValue(respostas);
+
+            Toast.makeText(ComentariosActivity.this, "Resposta Enviada", Toast.LENGTH_LONG).show();
+
+
+        } else {
+            Toast.makeText(ComentariosActivity.this, "Preencha o Campo", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
     public void ids() {
-        buttonComentarios = findViewById(R.id.buttonComentario);
+        //buttonComentarios = findViewById(R.id.buttonComentario);
         titulo = findViewById(R.id.titulo);
+        buttonEnviar = findViewById(R.id.buttonEnvivarTeste);
+        textComentario = findViewById(R.id.textComentarioTeste);
+
     }
 
 
